606a212ad84d16c8ac4d4bd90c7bd23b9f509554
Yackeim Folkes <yackeimf@gmail.com>
2021-10-24 18:18:14 -0500

    Updated JS File

--- info2180-project1-starter-master/scripts.js
+++ info2180-project1-starter-master/scripts.js
@@ -1,1 +1,17 @@
 /* Add your JavaScript to this file */
+function  subscription(){
+    var button = document.getElementsByTagName("button")[0];
+    var regexp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+    button.addEventListener("click",function(e){
+        var email= document.getElementById("email").value;
+        e.preventDefault();
+        if(email.match(regexp)){
+            document.getElementsByClassName("message")[0].innerHTML = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        }
+        else{
+            document.getElementsByClassName("message")[0].innerHTML = "Please enter a valid email address.";
+        }
+    });
+}
+window.onload=subscription;
